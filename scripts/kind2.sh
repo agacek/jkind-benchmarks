35581997bf4ec1e8bd7348639875083e23af1e4e
Andrew Gacek <andrew.gacek@gmail.com>
2018-01-30 09:48:51 -0600

    Ready to run

--- scripts/kind2.sh
+++ scripts/kind2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-KIND2=/usr/lfs/v0/gacek/kind2-v1.1.0-linux
+KIND2=~/kind2-v1.1.0-linux
 
 exec $KIND2 "$@"
--- scripts/nuxmv.sh
+++ scripts/nuxmv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-NUXMV=/usr/lfs/v0/gacek/nuXmv-1.1.1-Linux/bin/nuXmv
+NUXMV=nuXmv
 
 exec $NUXMV -source ./scripts/nuxmv-commands "$@"
