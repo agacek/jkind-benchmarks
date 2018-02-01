ef83a85fc5c06b3b1602e665aa902838ce02dcc7
Andrew Gacek <andrew.gacek@gmail.com>
2018-01-31 19:54:10 -0600

    Update processing

--- results/process.py
+++ results/process.py
@@ -6,12 +6,26 @@ if not os.path.isdir('processed'):
 
 for filename in ['nuxmv.txt', 'z3.txt', 'kind2.txt', 'jkind.txt', 'zustre.txt']:
     times = []
+
     with open(filename) as raw:
-        for line in raw.readlines():
+        lines = raw.readlines()
+        for i, line in enumerate(lines):
             if line.startswith('real'):
                 time = float(line[5:].strip())
+
+                # nuXmv time is sometimes reported just under 60 even though it failed
+                if time >= 59.9:
+                    time = 60
+
+                while '######' not in lines[i]:
+                    # Check for Zustre fatal errors
+                    if 'Fatal error' in lines[i]:
+                        time = 60
+                    i -= 1
+
                 if time < 60:
                     times.append(time)
+
     times.sort()
 
     with open('processed/' + filename, 'w') as out:
